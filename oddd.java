a1422263d2e822c54e82b819523d77f5ee374436
aishwarya003 <aishwarya.t2311@gmail.com>
2016-12-20 15:49:59 +0530

    Add files via upload

---
+++ oddd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java util.scanner;
+class number
+{
+int num;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+num=s.nextInt(System.in);
+if(num%2==0)
+System.out.print("ODD NUMBER");
+else
+System.out.println("EVEN NUMBER");
+}
