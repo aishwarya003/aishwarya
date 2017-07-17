9ae2324ae150ade9b2c207162c60ec7d06196c80
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-17 15:30:29 +0530

    Add files via upload

---
+++ Asciivaluesusingloops.java
@@ -0,0 +1,11 @@
+package sample;
+import java.util.*;
+public class Asciivaluesusingloops {
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	
+	for(int i=1;i<188;i++){
+	System.out.println("Character:"+(char)(i));
+	}
+}
+}
