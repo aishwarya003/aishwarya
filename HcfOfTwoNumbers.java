245b48eb2c3f6d830d046d64bc95d06a75228f8c
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-17 14:14:21 +0530

    Add files via upload

--- HcfOfTwoNumbers.java
+++ HcfOfTwoNumbers.java
@@ -3,18 +3,18 @@ import java.util.*;
 public class HcfOfTwoNumbers {
 public static void main(String[] args){
 	Scanner s=new Scanner(System.in);
-	int a=s.nextInt();
-	int b=s.nextInt();
-	System.out.println("G.C.D is"+hcf(a,b));
+	int m=s.nextInt();
+	int n=s.nextInt();
+	System.out.println("G.C.D is "+hcf(m,n));
 	s.close();
 }
 
-public static int hcf(int a,int b){
-	if(b==0){
-		return a;
+public static int hcf(int m,int n){
+	if(n==0){
+		return m;
 	}
 	else{
-		return (hcf(b,a%b));
+		return (hcf(n,m%n));
 	}
 }
 }
