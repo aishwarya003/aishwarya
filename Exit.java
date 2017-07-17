be976cd4aff17e1925c026106b616ce70cad6a26
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-17 19:21:18 +0530

    Add files via upload

---
+++ Exit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class Quit{
+	public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	char ss=s.next().charAt(0);
+	if(ch=='q'||ch=='Q'){
+		System.exit(0);
+	}
+	else{
+		System.out.println(ch);
+	}
+	}
+}
